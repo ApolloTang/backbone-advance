7019c55e176f36c4876a5daf3aa75301aa4797c6
Apollo Tang <tang.apollo@gmail.com>
2014-12-05 12:26:58 -0500

    at video 02.1-03:07 added my note on validate

--- validations/public/app.js
+++ validations/public/app.js
@@ -3,4 +3,22 @@ var User = Backbone.Model.extend({
         name: "",
         age: 0
     }
-})
+    , validate : function( attrs ) {
+        debugger;
+        if (attrs.age < 0 ){
+            return "age must be 0 or greater";
+        }
+        if (attrs.name === "") {
+            return "must have a name";
+        }
+    }
+});
+/*
+    u = new User
+
+    By default validate is called before save, but can also be called before set if {validate:true} is passed.
+
+    > u.set({name: ""},{validate:true})
+    > false
+
+*/
